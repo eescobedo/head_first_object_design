654b37331a6c3cfdf0cc8955584ed900a999e486
Enner Escobedo <enner.escobedo@nagarro.com>
2024-01-31 17:22:20 -0600

    Final changes using delegate on classes searching loosely coupled objects

--- chapter01/GuitarSpec.java
+++ chapter01/GuitarSpec.java
@@ -1,15 +1,17 @@
 public class GuitarSpec {
 
-    Builder builder;
-    String model;
-    Type type;
-    Wood backWood;
-    Wood topWood;
+    private Builder builder;
+    private String model;
+    private Type type;
+    private Wood backWood;
+    private Wood topWood;
+    private int numStrings;
 
-    public GuitarSpec(Builder builder, String model, Type type, Wood backWood, Wood topWood) {
+    public GuitarSpec(Builder builder, String model, Type type, int numStrings, Wood backWood, Wood topWood) {
         this.builder = builder;
         this.model = model;
         this.type = type;
+        this.numStrings = numStrings;
         this.backWood = backWood;
         this.topWood = topWood;
     }
@@ -32,6 +34,36 @@ public class GuitarSpec {
 
     public Wood getTopWood() {
         return topWood;
+    }
+
+    public boolean matches(GuitarSpec otherSpec) {
+
+        // ignore serial number since that's unique
+        // Ignore price since that's unique
+        if (builder != otherSpec.builder)
+            return false;
+
+        if ((model != null) && (!model.equals("")) && (!model.equals(otherSpec.model))) {
+            return false;
+        }
+
+        if (type != otherSpec.type) {
+            return false;
+        }
+
+        if (numStrings != otherSpec.numStrings) {
+            return false;
+        }
+
+        if (backWood != otherSpec.backWood){
+            return false;
+        }
+
+        if (topWood != otherSpec.topWood) {
+            return false;
+        }
+
+        return true;
     }
 
 }
